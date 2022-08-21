7d380902a6d11c84d9262c007ad105b929346e93
EC2 Default User <ec2-user@ip-172-31-14-195.ap-southeast-1.compute.internal>
2022-08-21 10:36:09 +0000

    main3

---
+++ run_test.py
@@ -0,0 +1,11 @@
+import time 
+list1 = []
+list2 = []
+start = time.time_ns()
+for i in range(50000):
+    stop = time.time_ns()
+    list1.append(i)
+    list2.append((stop-start))
+file = open('cpu_test_ec2_nocredit.txt', 'w')
+file.write(str(list2))
+file.close()
